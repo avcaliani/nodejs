7800585e95229a20efe90e01e04e60a47362610b
Anthony <avcaliani.it@gmail.com>
2018-12-30 18:02:31 -0200

    Adding Order Controller and some code fixes.

--- rest-shop/api/routes/order.js
+++ rest-shop/api/routes/order.js
@@ -3,45 +3,33 @@
  * @contact     https://github.com/avcaliani
  * 
  * @Description
- * Product Controller.
+ * Order Controller.
  */
 const Express = require('express');
 const Router = Express.Router();
 
 Router.get('/', (request, response, next) => {
     response.status(200).json({
-        message: 'Product:get() Works!'
+        message: 'Order:get() Works!'
     });
 });
 
 Router.get('/:id', (request, response, next) => {
     response.status(200).json({
-        message: 'Product:get(id) Works!',
+        message: 'Order:get(id) Works!',
         id: request.params.id
     });
 })
 
 Router.post('/', (request, response, next) => {
     response.status(200).json({
-        message: 'Product:post() Works!'
-    });
-});
-
-Router.patch('/:id', (request, response, next) => {
-    response.status(200).json({
-        message: 'Product:patch(id) Works!',
-        id: request.params.id
+        message: 'Order:post() Works!'
     });
 });
 
 Router.delete('/:id', (request, response, next) => {
-    /**
-     * You can even 'return' the response statement, but it is optional.
-     * For example:
-     *  return response.status(200).json({ ... });
-     */
     response.status(200).json({
-        message: 'Product:delete(id) Works!',
+        message: 'Order:delete(id) Works!',
         id: request.params.id
     });
 });
--- rest-shop/app.js
+++ rest-shop/app.js
@@ -10,8 +10,10 @@ const App = Express();
 
 // Controllers
 const Product = require('./api/routes/product');
+const Order = require('./api/routes/order');
 
 // Registering Controllers
-App.use('/products', Product)
+App.use('/products', Product);
+App.use('/orders', Order);
 
 module.exports = App;
