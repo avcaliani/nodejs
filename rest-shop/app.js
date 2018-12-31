7c5dc2e8ad764a99d124821aa250ecd1ab918f2e
Anthony Caliani <avcaliani.it@gmail.com>
2018-12-30 23:30:03 -0200

    Adding 'morgan' package, which is going to log some data automatically.

--- rest-shop/app.js
+++ rest-shop/app.js
@@ -6,11 +6,16 @@
  * App JS File.
  */
 const Express = require('express');
-const App = Express();
+const Morgan = require('morgan'); // Log Handler
 
 // Controllers
 const Product = require('./api/routes/product');
 const Order = require('./api/routes/order');
+
+const App = Express();
+
+// Registering Log Handler
+App.use(Morgan('dev'));
 
 // Registering Controllers
 App.use('/products', Product);
