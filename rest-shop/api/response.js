1974f057ef98bb098c5bdf964aa74318c99f735f
Anthony <avcaliani.it@gmail.com>
2019-01-01 19:12:24 -0200

    Updating responses methods.

--- rest-shop/api/response.js
+++ rest-shop/api/response.js
@@ -5,28 +5,23 @@
  * @Description
  * Common Response Structure.
  */
-const Response = {
 
-  ok: function (response, data, code = 200) {
-    /**
-     * You can even 'return' the response statement, but it is optional.
-     * For example:
-     *  return response.status(200).json({ ... });
-     */
-    response.status(data === null ? 404 : code).json({
-      data: data,
-      message: data === null ? 'Data not found' : null
-    });
-  },
+exports.ok = (response, data, code = 200) => {
+  /**
+   * You can even 'return' the response statement, but it is optional.
+   * For example:
+   *  return response.status(200).json({ ... });
+   */
+  response.status(data === null ? 404 : code).json({
+    data: data,
+    message: data === null ? 'Data not found' : null
+  });
+}
 
-  error: function (response, err, code = 500) {
-    console.error('ERROR!', err);
-    response.status(code).json({
-      data: null,
-      message: err.message || err
-    });
-  }
-
-};
-
-module.exports = Response;
+exports.error = (response, err, code = 500) => {
+  console.error('ERROR!', err);
+  response.status(code).json({
+    data: null,
+    message: err.message || err
+  });
+}
