f3b137300b031939846bb8735f056ffeaf4b3a4b
Anthony <avcaliani.it@gmail.com>
2019-01-03 22:40:26 -0200

    Working with Product Controller

--- rest-shop/api/controllers/product.controller.js
+++ rest-shop/api/controllers/product.controller.js
@@ -5,6 +5,7 @@
  * @Description
  * Product Controller.
  */
+const Mongoose = require('mongoose');
 const Product = require('../models/product.model');
 
 /**
@@ -26,6 +27,90 @@ exports.findAll = () => {
         resolve(data);
       })
       .catch(reject);
+  });
+};
+
+/**
+ * Find a product by id.
+ * @param {*} id Product ID.
+ * @return {Promise} Product or Error.
+ */
+exports.find = (id) => {
+  return new Promise((resolve, reject) => {
+    Product.findById(id)
+    .exec()
+    .then(result => resolve(parse(result)))
+    .catch(reject)
+  });
+};
+
+/**
+ * Save new product.
+ * @param {*} product Product.
+ * @return {Promise} Product or Error.
+ */
+exports.save = (product) => {
+  return new Promise((resolve, reject) => {
+
+    if (!product)
+      return reject('A Product object is required.');
+    if (!product.name)
+      return reject('A Product Name is required.');
+    if (!product.price)
+      return reject('A Product Price is required.');
+
+    const prod = new Product({
+      _id: new Mongoose.Types.ObjectId(),
+      name: product.name,
+      price: product.price,
+      image: product.image || null
+    });
+
+    prod.save()
+      .then(result => resolve(parse(result)))
+      .catch(reject);
+  });
+};
+
+/**
+ * Update product data.
+ * @param {*} product Product.
+ * @return {Promise} true or Error.
+ */
+exports.update = (id, product) => {
+  return new Promise((resolve, reject) => {
+
+    if (!id) return reject('Product ID is required.');
+    if (!product) return reject('Product Object is required.');
+    
+    const prod = { };
+    if (product.name) prod.name = product.name;
+    if (product.price) prod.price = product.price;
+
+    Product.update({ _id: id }, { $set: prod })
+    .exec()
+    .then(result => {
+      if (result.n > 0) resolve(true);
+      else reject('Product not found.');
+    })
+    .catch(reject);
+  });
+};
+
+/**
+ * Remove product by id.
+ * @param {*} id Product ID.
+ * @return {Promise} true or Error.
+ */
+exports.remove = (id) => {
+  return new Promise((resolve, reject) => {
+    Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      if (result.n > 0) resolve(true);
+      else reject('Product not found');
+    })
+    .catch(err => Response.error(response, err));
   });
 };
 
--- rest-shop/api/routes/product.routes.js
+++ rest-shop/api/routes/product.routes.js
@@ -7,12 +7,10 @@
  */
 const Express = require('express');
 const Router = Express.Router();
-const Mongoose = require('mongoose');
-const AuthHandler = require('../middlewares/auth.handler')
-const FileHandler = require('../middlewares/file-upload.handler');
 const Response = require('../response');
 
-const Product = require('../models/product.model');
+const AuthHandler = require('../middlewares/auth.handler')
+const FileHandler = require('../middlewares/file-upload.handler');
 
 const ProductController = require('../controllers/product.controller');
 
@@ -24,81 +22,49 @@ Router.get('/', (request, response, next) => {
 });
 
 Router.get('/:id', (request, response, next) => {
-  Product.findById(request.params.id)
-    .exec()
-    .then(result => Response.ok(response, parse(result)))
-    .catch(err => Response.error(response, err, 500));
+  ProductController.find(request.params.id).then(
+    products => Response.ok(response, products),
+    err => Response.error(response, err)
+  );
 })
 
 Router.post('/', AuthHandler, FileHandler.upload.single('image'), (request, response, next) => {
   
   if (typeof request.file === 'undefined')
-    return Response.error(response, 'Invalid file')
+    return Response.error(response, 'Invalid file.')
 
-  const product = new Product({
-    _id: new Mongoose.Types.ObjectId(),
+  const product = {
     name: request.body.name,
     price: request.body.price,
     image: request.file.path
-  });
+  };
 
-  product.save()
-    .then(result => Response.ok(response, parse(result), 201))
-    .catch(err => Response.error(response, err));
+  ProductController.save(product).then(
+    product => Response.ok(response, product, 201),
+    err => Response.error(response, err)
+  );
 });
 
 Router.patch('/:id', AuthHandler, (request, response, next) => {
 
   if (!Array.isArray(request.body))
-    return Response.error(response, 'Invalid product body', 500);
+    return Response.error(response, 'Invalid product body.');
 
   const data = {};
   for (const prop of request.body)
     data[prop.name] = prop.value;
 
-  Product.update({ _id: request.params.id }, { $set: data })
-    .exec()
-    .then(result => {
-      if (result.n > 0)
-        return Response.ok(response, true);
-      Response.error(response, 'Product not found', 404);
-    })
-    .catch(err => Response.error(response, err));
+  ProductController.update(request.params.id, data).then(
+    status => Response.ok(response, status),
+    err => Response.error(response, err)
+  );
 });
 
 Router.delete('/:id', AuthHandler, (request, response, next) => {
-  Product.remove({ _id: request.params.id })
-    .exec()
-    .then(result => {
-      if (result.n > 0)
-        return Response.ok(response, true);
-      Response.error(response, 'Product not found', 404);
-    })
-    .catch(err => Response.error(response, err));
+  ProductController.remove(request.params.id).then(
+    status => Response.ok(response, status),
+    err => Response.error(response, err)
+  );
 });
-
-/**
- * Clean up a product object, which was returned from database.
- * Request Object is optional and would have 'type' and 'url' fields.
- * @param {*} product Product Object.
- * @param {*} request Request Object (Optional)
- */
-function parse(product, request = null) {
-
-  if (product === null || typeof product === 'undefined')
-    return null;
-
-  const prod = {
-    id: product._id,
-    name: product.name,
-    price: product.price,
-    image: product.image
-  };
-
-  if (request !== null)
-    prod.request = request;
-
-  return prod;
-}
 
 module.exports = Router;
